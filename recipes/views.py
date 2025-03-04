4d2c2376ca218b4174a6f74574c419518f670ff2
jedson Ferreira <jedazevedo@gmail.com>
2025-03-04 11:37:23 -0300

    atualizando com arquivo factory

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     # http response
-    return render(request, 'recipes\\pages\\home.html', context={'name': 'Jedson'})
+    return render(request, 'recipes\\pages\\home.html', context={
+        'recipes': [make_recipe() for _ in range(6)]})
 
 def recipe(request, id):
     # http response
-    return render(request, 'recipes\\pages\\recipe_view.html')
+    return render(request, 'recipes\\pages\\recipe_view.html', context={
+        'recipe': make_recipe(),
+        'is_detail': True})
 
 
 
