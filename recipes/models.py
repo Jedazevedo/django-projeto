e36a7f63dfb8a6b21f6d1e53b11f575a1c9c59e1
jedson Ferreira <jedazevedo@gmail.com>
2025-03-19 23:02:35 -0300

    incluindo arquivo models

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model): # visualizar isso como se fosse uma tabela sql
+      title = models.CharField(max_length=65) # varchar
+      description = models.CharField(max_length=165)
+      slug = models.models.SlugField()
+      preparation_time = models.IntegerField()
+      preparation_time_uni = models.CharField(max_length=65)
+      servings_time = models.IntegerField()
+      servings_time_uni = models.CharField(max_length=65)
+      preparation_steps = models.TextField()
+      preparation_steps_is_html = models.BooleanField(default=False)
+      created_at = models.DateField(auto_now_add=True) # vai pegar a data automaticamente
+      update_at = models.DateField(auto_now=True)
+      is_published = models.BooleanField(default=False)
+      cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+
+
+      pass
--- recipes/views.py
+++ recipes/views.py
@@ -6,13 +6,13 @@ from utils.recipes.factory import make_recipe
 def home(request):
     # http response
     return render(request, 'recipes\\pages\\home.html', context={
-        'recipes': [make_recipe() for _ in range(6)]})
+        'recipes': [make_recipe() for _ in range(3)]})
 
 def recipe(request, id):
     # http response
     return render(request, 'recipes\\pages\\recipe_view.html', context={
         'recipe': make_recipe(),
-        'is_detail': True})
+        'is_detail_page': True})
 
 
 
